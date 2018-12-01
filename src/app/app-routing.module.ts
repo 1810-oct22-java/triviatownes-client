47aa09c3a27b8e67eca7a7dbea3d5f8a1768ca1f
Joseph Ross <joseph.ross.11211@gmail.com>
2018-12-01 00:10:06 -0500

    Client is ready for development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//ROUTES: EXAMPLE
+/*
+const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'manager', component: ManagerComponent}
+];
+*/
+
 const routes: Routes = [];
 
 @NgModule({
