3b0f1f80eaffbde0ba3c62ef92556b05dcac6fcc
Ian Barney <ianbarney23@gmail.com>
2018-12-02 13:56:47 -0500

    Ajax functions. Going to swap out the buttons with Michael's when he's done.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
   categories = ['Science', 'Art', 'History', 'Sports', 'Nature', 'Geo', 'Cars', 'Lit', 'New'];
+  
   constructor() { }
 
   ngOnInit() {
@@ -15,6 +17,7 @@ export class LandingPageComponent implements OnInit {
 
   //call when a user hits landing page
   newUser(){
+    
     $.ajax({
       url: "/", success: function (result) {
         //load lobby
@@ -22,21 +25,39 @@ export class LandingPageComponent implements OnInit {
     });
   }
 
+  /*
+  * Sends pin to servlet
+  */
   joinLobby() {
+    var pin = $('#pin').val();
+    console.log(pin);
     $.ajax({
-      url: "/connect-to-lobby", success: function (result) {
-        //load lobby
-      }
-    });
+      url: "lobby",
+      method: "POST",
+      data: {pin}
+    }).then(function successCallback(response) {
+      //load lobby
+      
+  }, function errorCallback(response) {
+      //Send user wrong pin error message
+      alert("A room with that pin does not exist");
+});
   }
-  pickCategory() {
+  pickCategory(cat) {
+    console.log("picking cat " + cat);
     //get cat and send it in post
 
     $.ajax({
-      url: "tbd", success: function (result) {
-        //create and load lobby based on cat
-      }
-    });
+      url: "selectLobby",
+      method: "POST",
+      data: {cat}
+    }).then(function successCallback(response) {
+      //load lobby
+      
+  }, function errorCallback(response) {
+      //Send user wrong pin error message
+      alert("There was a problem finding that room");
+});
   }
 
 
