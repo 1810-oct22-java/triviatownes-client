207fb6620dcc8e2d910de7fab3bc1a08dcd7ba97
Joseph Ross <joseph.ross.11211@gmail.com>
2018-12-04 21:38:05 -0500

    full working front end websocket example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,32 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LeaderboardPageComponent } from './leaderboard-page/leaderboard-page.component';
 import { ServerLobbyComponent } from './server-lobby/server-lobby.component';
 import { WaitingPageComponent } from './waiting-page/waiting-page.component';
+import { SocketTestComponent } from './socket-test/socket-test.component';
+
+import { StompConfig, StompService } from '@stomp/ng2-stompjs';
+
+/*
+const stompConfig: StompConfig = {
+  // Which server?
+  url: "ws://127.0.0.1:8080/WebSocketExample/chat",
+  headers: {
+    test: "Hello"
+  },
+
+  // How often to heartbeat?
+  // Interval in milliseconds, set to 0 to disable
+  heartbeat_in: 0, // Typical value 0 - disabled
+  heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+
+  // Wait in milliseconds before attempting auto reconnect
+  // Set to 0 to disable
+  // Typical value 5000 (5 seconds)
+  reconnect_delay: 5000,
+
+  // Will log diagnostics on console
+  debug: true
+};
+*/
 
 @NgModule({
   declarations: [
@@ -22,7 +48,8 @@ import { WaitingPageComponent } from './waiting-page/waiting-page.component';
     ServerLobbyComponent,
     LandingPageComponent,
     LeaderboardPageComponent,
-    WaitingPageComponent
+    WaitingPageComponent,
+    SocketTestComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +58,27 @@ import { WaitingPageComponent } from './waiting-page/waiting-page.component';
     DataTablesModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+
+    /*
+    StompService,
+    {
+      provide: StompConfig,
+      useValue: stompConfig
+    }
+    */
+
+    // {
+    //   provide: InjectableRxStompConfig,
+    //   useValue: myRxStompConfig
+    // },
+    // {
+    //   provide: RxStompService,
+    //   useFactory: rxStompServiceFactory,
+    //   deps: [InjectableRxStompConfig]
+    // }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
