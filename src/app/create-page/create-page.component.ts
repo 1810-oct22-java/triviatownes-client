c2f1ca75088e9ea7a9fe91f9345b17bd54ce6ce9
Michael Craig <michaelcraig@Michaels-MacBook-Pro.local>
2018-12-05 08:59:20 -0500

    messing with hovers and animejs

--- src/app/create-page/create-page.component.ts
+++ src/app/create-page/create-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-create-page',
@@ -51,10 +52,10 @@ export class CreatePageComponent implements OnInit {
           private: x.private,
           name: x.name
         },
-        success: function(res) {
+        success: function (res) {
           console.log('** GAME CREATED **');
         },
-        error: function(res) {
+        error: function (res) {
           alert('There was a problem connecting to lobby...');
         }
       });
@@ -67,6 +68,108 @@ export class CreatePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const penner = anime.timeline();
+    // penner
+    //   .add({
+    //     targets: '#penner .linear', translateY: 250, offset: 0,
+    //     easing: 'linear'
+    //   })
+    //   .add({
+    //     targets: '#penner .InQuad', translateY: 250, offset: 0,
+    //     easing: 'easeInQuad'
+    //   })
+    //   .add({
+    //     targets: '#penner .InCubic', translateY: 250, offset: 0,
+    //     easing: 'easeInCubic'
+    //   })
+    //   .add({
+    //     targets: '#penner .InQuart', translateY: 250, offset: 0,
+    //     easing: 'easeInQuart'
+    //   })
+    //   .add({
+    //     targets: '#penner .InQuint', translateY: 250, offset: 0,
+    //     easing: 'easeInQuint'
+    //   })
+    //   .add({
+    //     targets: '#penner .InSine', translateY: 250, offset: 0,
+    //     easing: 'easeInSine'
+    //   })
+    //   .add({
+    //     targets: '#penner .InExpo', translateY: 250, offset: 0,
+    //     easing: 'easeInExpo'
+    //   })
+    //   .add({
+    //     targets: '#penner .InCirc', translateY: 250, offset: 0,
+    //     easing: 'easeInCirc'
+    //   })
+    //   .add({
+    //     targets: '#penner .InBack', translateY: 250, offset: 0,
+    //     easing: 'easeInBack'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutQuad', translateY: 250, offset: 0,
+    //     easing: 'easeOutQuad'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutCubic', translateY: 250, offset: 0,
+    //     easing: 'easeOutCubic'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutQuart', translateY: 250, offset: 0,
+    //     easing: 'easeOutQuart'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutQuint', translateY: 250, offset: 0,
+    //     easing: 'easeOutQuint'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutSine', translateY: 250, offset: 0,
+    //     easing: 'easeOutSine'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutExpo', translateY: 250, offset: 0,
+    //     easing: 'easeOutExpo'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutCirc', translateY: 250, offset: 0,
+    //     easing: 'easeOutCirc'
+    //   })
+    //   .add({
+    //     targets: '#penner .OutBack', translateY: 250, offset: 0,
+    //     easing: 'easeOutBack'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutQuad', translateY: 250, offset: 0,
+    //     easing: 'easeInOutQuad'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutCubic', translateY: 250, offset: 0,
+    //     easing: 'easeInOutCubic'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutQuart', translateY: 250, offset: 0,
+    //     easing: 'easeInOutQuart'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutQuint', translateY: 250, offset: 0,
+    //     easing: 'easeInOutQuint'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutSine', translateY: 250, offset: 0,
+    //     easing: 'easeInOutSine'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutExpo', translateY: 250, offset: 0,
+    //     easing: 'easeInOutExpo'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutCirc', translateY: 250, offset: 0,
+    //     easing: 'easeInOutCirc'
+    //   })
+    //   .add({
+    //     targets: '#penner .InOutBack', translateY: 250, offset: 0,
+    //     easing: 'easeInOutBack'
+    //   });
   }
 
 }
